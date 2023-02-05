f389713e5a6adb6b245e4834bc8d1b365d7add59
Joszko <jakub.rezler96@gmail.com>
2023-02-06 00:21:55 +0100

    update Stats.js and it's styles

--- src/pages/homeComponents/Stats.js
+++ src/pages/homeComponents/Stats.js
@@ -1,6 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { useSpring, animated } from "react-spring";
+import Media from "react-media";
 import "../../style/css/stats.css";
+
+const useScrollPosition = () => {
+	const [scrollPosition, setScrollPosition] = useState(0);
+
+	useEffect(() => {
+		const updatePosition = () => {
+			setScrollPosition(window.pageYOffset);
+		};
+		window.addEventListener("scroll", updatePosition);
+		updatePosition();
+		return () => window.removeEventListener("scroll", updatePosition);
+	}, []);
+
+	return scrollPosition;
+};
 
 const Number = ({ n }) => {
 	const { num } = useSpring({
@@ -16,44 +32,89 @@ const Number = ({ n }) => {
 		</animated.div>
 	);
 };
-
 const Stats = () => {
 	const [scrolled, setScrolled] = useState(false);
+	let scrollPosition = useScrollPosition();
+	console.log(scrollPosition);
+	if (scrolled === false && scrollPosition >= 150) {
+		setScrolled(true);
+	} else if (scrolled === true && scrollPosition < 150) {
+		setScrolled(false);
+	}
+	// useEffect(() => {
+	// const scrollHandler = () => {
 
-	useEffect(() => {
-		const scrollHandler = () => {
-			// console(window.scrollY);
-			if (scrolled === false && window.scrollY >= 150) {
-				setScrolled(true);
-				// window.removeEventListener("scroll", scrollHandler);
-			} else if (scrolled === true && window.scrollY < 150) {
-				setScrolled(false);
-			}
-		};
-		window.addEventListener("scroll", scrollHandler);
-	}, [scrolled]);
+	//
+	// };
+	// scrollPosition.addEventListener("scroll", scrollHandler);
+	// }, [scrolled]);
 
 	return (
-		<section className="stats-container" id="section1">
-			<div className="first-stat stats">
-				<span className="stat-number">{scrolled ? <Number n={26} /> : 0}</span>
-				<span className="stat-description">tłumaczonych języków</span>
-			</div>
-			<div className="second-stat stats">
-				<span className="stat-number">{scrolled ? <Number n={21} /> : 0}</span>
-				<span className="stat-description">lat doświadczenia</span>
-			</div>
-			<div className="third-stat stats">
-				<span className="stat-number thousand">
-					{scrolled ? <Number n={1000} /> : 0}+
-				</span>
-				<span className="stat-description">zadowolonych klientów</span>
-			</div>
-			<div className="forth-stat stats">
-				<span className="stat-number">{scrolled ? <Number n={16} /> : 0}</span>
-				<span className="stat-description">współpracujących partnerów</span>
-			</div>
-		</section>
+		<>
+			<Media query="(min-width: 992px)">
+				{(matches) => {
+					return matches ? (
+						// desktop
+						<section className="stats-container" id="section1">
+							<div className="first-stat stats">
+								<span className="stat-number">
+									{scrolled ? <Number n={26} /> : 0}
+								</span>
+								<span className="stat-description">tłumaczonych języków</span>
+							</div>
+							<div className="second-stat stats">
+								<span className="stat-number">
+									{scrolled ? <Number n={21} /> : 0}
+								</span>
+								<span className="stat-description">lat doświadczenia</span>
+							</div>
+							<div className="third-stat stats">
+								<span className="stat-number thousand">
+									{scrolled ? <Number n={1000} /> : 0}+
+								</span>
+								<span className="stat-description">zadowolonych klientów</span>
+							</div>
+							<div className="forth-stat stats">
+								<span className="stat-number">
+									{scrolled ? <Number n={16} /> : 0}
+								</span>
+								<span className="stat-description">
+									współpracujących partnerów
+								</span>
+							</div>
+						</section>
+					) : (
+						// mobile
+						<section className="stats-container" id="section1">
+							<div className="first-block-stats block">
+								<div className="first-stat stats">
+									<span className="stat-number">26</span>
+									<span className="stat-description">tłumaczonych języków</span>
+								</div>
+								<div className="second-stat stats">
+									<span className="stat-number">21</span>
+									<span className="stat-description">lat doświadczenia</span>
+								</div>
+							</div>
+							<div className="second-block-stats block">
+								<div className="third-stat stats">
+									<span className="stat-number thousand">1000+</span>
+									<span className="stat-description">
+										zadowolonych klientów
+									</span>
+								</div>
+								<div className="forth-stat stats">
+									<span className="stat-number">16</span>
+									<span className="stat-description">
+										współpracujących partnerów
+									</span>
+								</div>
+							</div>
+						</section>
+					);
+				}}
+			</Media>
+		</>
 	);
 };
 
