584584539005c4ba363ea199ee17615c1fffea33
Joszko <jakub.rezler96@gmail.com>
2023-02-18 21:37:46 +0100

    update Values.js and styles

--- src/pages/homeComponents/Values.js
+++ src/pages/homeComponents/Values.js
@@ -6,23 +6,23 @@ const Values = () => {
 		<section className="values-we-offer-container" id="section2">
 			<h3>Nasze wartości</h3>
 			<div className="values-box">
-				<div className="value">
+				<div className="value value1">
 					<div className="value-one value-img"></div>
 					<span>Rzetelność</span>
 				</div>
-				<div className="value exper">
+				<div className="value value2 exper">
 					<div className="value-two value-img"></div>
-					<span>Doświadczenie</span>
+					<span className="upper-span">Doświadczenie</span>
 				</div>
-				<div className="value">
+				<div className="value value3">
 					<div className="value-three value-img"></div>
 					<span>Profesjonalizm</span>
 				</div>
-				<div className="value">
+				<div className="value value4">
 					<div className="value-four value-img"></div>
-					<span>Terminowość</span>
+					<span className="lower-span">Terminowość</span>
 				</div>
-				<div className="value">
+				<div className="value value5">
 					<div className="value-five value-img"></div>
 					<span>Partnerstwo</span>
 				</div>
