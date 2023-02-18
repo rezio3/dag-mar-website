7f68d4522e2b3311fbea3336c8ce147e504a03db
Joszko <jakub.rezler96@gmail.com>
2023-02-18 02:46:16 +0100

    modify MenuInBurgerIcon.js and add styles

--- src/layouts/MenuInBurgerIcon.js
+++ src/layouts/MenuInBurgerIcon.js
@@ -1,22 +1,35 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "../style/css/MenuInBurgerIcon.css";
 
-const MenuInBurgerIcon = () => {
+const MenuInBurgerIcon = (props) => {
+	const handleClick = () => {
+		props.setBurgerActive(false);
+	};
 	return (
 		<div className="burger-nav-content">
-			<NavLink to="/" end className="m-nav-btn-desktop">
-				<button className="m-navbtn">O nas</button>
-			</NavLink>
-			<NavLink to="/offer" className="m-nav-btn-desktop">
-				<button className="m-navbtn">Oferta</button>
-			</NavLink>
-			<NavLink to="/fortranslators" className="nav-btn-desktop">
-				<button className="m-navbtn">Dla tłumaczy</button>
-			</NavLink>
-			<NavLink to="/faq" className="m-nav-btn-desktop">
-				<button className="m-faq m-navbtn">FAQ</button>
-			</NavLink>
-			<div className="m-language-btn">
+			<div className="buttons-container">
+				<NavLink to="/" end className="m-nav-btn-desktop">
+					<div className="button-container" onClick={handleClick}>
+						<button className="m-navbtn">O nas</button>
+					</div>
+				</NavLink>
+				<NavLink to="/offer" className="m-nav-btn-desktop">
+					<div className="button-container" onClick={handleClick}>
+						<button className="m-navbtn">Oferta</button>
+					</div>
+				</NavLink>
+				<NavLink to="/fortranslators" className="nav-btn-desktop">
+					<div className="button-container" onClick={handleClick}>
+						<button className="m-navbtn">Dla tłumaczy</button>
+					</div>
+				</NavLink>
+				<NavLink to="/faq" className="m-nav-btn-desktop">
+					<div className="button-container" onClick={handleClick}>
+						<button className="m-faq m-navbtn">FAQ</button>
+					</div>
+				</NavLink>
+				{/* <div className="m-language-btn">
 				<div className="m-only-btn">
 					<span>PL</span>
 					<span className="m-material-symbols-outlined">expand_more</span>
@@ -25,10 +38,13 @@ const MenuInBurgerIcon = () => {
 					<span className="m-eng-span">English</span>
 					<span className="m-de-span">Deutsch</span>
 				</div>
+			</div> */}
+				<NavLink to="/contact" className="m-nav-btn-desktop">
+					<button className="m-contact-button" onClick={handleClick}>
+						Skontaktuj się
+					</button>
+				</NavLink>
 			</div>
-			<NavLink to="/contact" className="m-nav-btn-desktop">
-				<button className="m-contact-button">Skontaktuj się</button>
-			</NavLink>
 		</div>
 	);
 };
