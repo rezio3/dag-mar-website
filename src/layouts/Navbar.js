fb4cf8af2df30b3eaa3fe6f329838a9149fbb562
Joszko <jakub.rezler96@gmail.com>
2023-02-18 02:19:50 +0100

    add props to Navbar.js

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -3,12 +3,19 @@ import Media from "react-media";
 import NavbarMobile from "./NavbarMobile";
 import NavbarDesktop from "./NavbarDesktop";
 
-const Navbar = () => {
+const Navbar = (props) => {
 	return (
 		<>
 			<Media query="(min-width: 992px)">
 				{(matches) => {
-					return matches ? <NavbarDesktop /> : <NavbarMobile />;
+					return matches ? (
+						<NavbarDesktop />
+					) : (
+						<NavbarMobile
+							burgerActive={props.burgerActive}
+							setBurgerActive={props.setBurgerActive}
+						/>
+					);
 				}}
 			</Media>
 		</>
