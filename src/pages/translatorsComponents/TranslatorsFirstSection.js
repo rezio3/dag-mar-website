9acc15dadfe1cbca118a0721e0d3875b09b2899a
Joszko <jakub.rezler96@gmail.com>
2023-02-18 23:16:19 +0100

    update text in TranslatorsFirstSection.js

--- src/pages/translatorsComponents/TranslatorsFirstSection.js
+++ src/pages/translatorsComponents/TranslatorsFirstSection.js
@@ -10,15 +10,10 @@ const TranslatorsFirstSection = () => {
 					<h2>Chętnie nawiążemy współpracę</h2>
 				</div>
 				<p className="translators-txt">
-					Biuro Tłumaczeń Przysięgłych „DAG-MAR” skupia wokół siebie zespół
-					Tłumaczy <br />z wieloletnim doświadczeniem. Wobec stale rosnących
-					wymagań i oczekiwań naszych Klientów decydujemy się na współpracę
-					tylko z prawdziwymi profesjonalistami w swojej branży.
-				</p>
-				<p className="translators-txt">
-					Zapewniamy materiały do tłumaczenia, uczciwe stawki za wykonaną pracę,
-					rozliczenia na czas, a także rekomendacje i polecenie innym
-					pracodawcom <br />w przyszłości.
+					Biuro Tłumaczeń Przysięgłych„DAG-MAR” skupia wokół siebie zespół
+					Tłumaczy z wieloletnim doświadczeniem. Wobec stale rosnących wymagań i
+					oczekiwań naszych Klientów decydujemy się na współpracę tylko z
+					prawdziwymi profesjonalistami w swojej branży.
 				</p>
 			</div>
 			<div className="translators-arc-img">
