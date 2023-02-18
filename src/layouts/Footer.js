9c588302ac28503525c6cc0b432dddd893435ee1
Joszko <jakub.rezler96@gmail.com>
2023-02-18 01:48:45 +0100

    rebuild whole mobile footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -58,45 +58,53 @@ const Footer = () => {
 					) : (
 						<footer className="footer-container">
 							<div className="upper-footer-container">
-								<img
-									src={footerLogo}
-									alt="dag-mar logo"
-									className="footer-logo"
-								/>
-								<div className="footer-address">
-									<h2>Biuro Tłumaczeń Przysięgłych DAG-MAR</h2>
-									<span className="footer-contact-number">+48 607 618 567</span>
-									<span>Karkonoska 1C, 59-300 Lubin</span>
+								<div className="footer-logo-container">
+									<img
+										src={footerLogo}
+										alt="dag-mar logo"
+										className="footer-logo"
+									/>
+									<div className="footer-address">
+										<h2>Biuro Tłumaczeń Przysięgłych DAG-MAR</h2>
+										<span className="footer-contact-number">
+											+48 607 618 567
+										</span>
+										<span>Karkonoska 1C, 59-300 Lubin</span>
+									</div>
 								</div>
-								<div className="aboutUs-offer-FAQ-translators-footer aboutUs-footer">
-									<NavLink to="/" end className="nav-btn-desktop">
-										<span className="footer-link">O nas</span>
-									</NavLink>
-									<NavLink to="/offer" className="nav-btn-desktop">
-										<span className="footer-link">Oferta</span>
-									</NavLink>
-									<span className="footer-link">Płatności</span>
+								<div className="middle-footer-container">
+									<div className="aboutUs-offer-FAQ-translators-footer aboutUs-footer">
+										<NavLink to="/" end className="nav-btn-desktop">
+											<span className="footer-link">O nas</span>
+										</NavLink>
+										<NavLink to="/offer" className="nav-btn-desktop">
+											<span className="footer-link">Oferta</span>
+										</NavLink>
+										<NavLink to="/contact" className="nav-btn-desktop">
+											<button className="footer-contact-btn">
+												Skontaktuj się
+											</button>
+										</NavLink>
+									</div>
+									<div className="aboutUs-offer-FAQ-translators-footer aboutUs-right">
+										<span className="footer-link">Płatności</span>
+										<NavLink to="/fortranslators" className="nav-btn-desktop">
+											<span className="footer-link">Dla Tłumaczy</span>
+										</NavLink>
+										<NavLink to="/faq" className="nav-btn-desktop">
+											<span className="footer-link">FAQ</span>
+										</NavLink>
+									</div>
 								</div>
-								<div className="aboutUs-offer-FAQ-translators-footer">
-									<NavLink to="/fortranslators" className="nav-btn-desktop">
-										<span className="footer-link">Dla Tłumaczy</span>
-									</NavLink>
-									<NavLink to="/faq" className="nav-btn-desktop">
-										<span className="footer-link">FAQ</span>
-									</NavLink>
-								</div>
-
-								<NavLink to="/contact" className="nav-btn-desktop">
-									<button className="footer-contact-btn">Skontaktuj się</button>
-								</NavLink>
 							</div>
 							<div className="lower-footer-container">
-								<span>Copyright &copy; 2022 DAG-MAR</span>
 								<div className="icons-footer-container">
 									<img src={fbIcon} alt="Facebook Icon" className="icon" />
 									<img src={googleIcon} alt="Google Icon" className="icon" />
 								</div>
+
 								<span>Polityka prywatności</span>
+								<span>Copyright &copy; 2022 DAG-MAR</span>
 							</div>
 						</footer>
 					);
