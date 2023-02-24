14699c1e943a37396ac806bb1952de2396c4308c
Joszko <jakub.rezler96@gmail.com>
2023-02-24 15:34:01 +0100

    update and add react-media to Fortranslators.js

--- src/pages/Fortranslators.js
+++ src/pages/Fortranslators.js
@@ -2,27 +2,56 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "../style/css/forTranslators.css";
 import TranslatorsFirstSection from "./translatorsComponents/TranslatorsFirstSection";
+import Media from "react-media";
 
 const Fortranslators = () => {
 	return (
-		<div className="for-translators-container">
-			<TranslatorsFirstSection />
-			<div className="second-section">
-				<h2>Czekamy na ciebie</h2>
-				<span>
-					Jeśli zna Pani/Pan perfekcyjnie jeden lub więcej języków obcych,
-					swobodnie posługuje się językiem polskim, ma przynajmniej kilkuletnie{" "}
-					<br />
-					doświadczenie w tłumaczeniu dokumentów, jest osobą odpowiedzialną,
-					solidną, dyspozycyjną, pracującą samodzielnie lub potrafiącą
-					<br /> włączyć się do zespołu i jest zainteresowana/y podjęciem
-					współpracy z naszym Biurem zapraszamy do kontaktu.
-				</span>
-				<NavLink to="/contact" className="nav-btn-desktop">
-					<button className="write-us-btn">Napisz do nas</button>
-				</NavLink>
-			</div>
-		</div>
+		<>
+			<Media query="(min-width: 992px)">
+				{(matches) => {
+					return matches ? (
+						<div className="for-translators-container">
+							<TranslatorsFirstSection />
+							<div className="second-section">
+								<h2>Czekamy na ciebie</h2>
+								<span>
+									Jeśli zna Pani/Pan perfekcyjnie jeden lub więcej języków
+									obcych, swobodnie posługuje się językiem polskim, ma
+									przynajmniej kilkuletnie <br />
+									doświadczenie w tłumaczeniu dokumentów, jest osobą
+									odpowiedzialną, solidną, dyspozycyjną, pracującą samodzielnie
+									lub potrafiącą
+									<br /> włączyć się do zespołu i jest zainteresowana/y
+									podjęciem współpracy z naszym Biurem zapraszamy do kontaktu.
+								</span>
+								<NavLink to="/contact" className="nav-btn-desktop">
+									<button className="write-us-btn">Napisz do nas</button>
+								</NavLink>
+							</div>
+						</div>
+					) : (
+						<div className="for-translators-container">
+							<TranslatorsFirstSection />
+							<div className="second-section">
+								<h2>Czekamy na ciebie</h2>
+								<span>
+									Jeśli zna Pani/Pan perfekcyjnie jeden lub więcej języków
+									obcych, swobodnie posługuje się językiem polskim, ma
+									przynajmniej kilkuletnie doświadczenie w tłumaczeniu
+									dokumentów, jest osobą odpowiedzialną, solidną, dyspozycyjną,
+									pracującą samodzielnie lub potrafiącą włączyć się do zespołu i
+									jest zainteresowana/y podjęciem współpracy z naszym Biurem
+									zapraszamy do kontaktu.
+								</span>
+								<NavLink to="/contact" className="nav-btn-desktop">
+									<button className="write-us-btn">Napisz do nas</button>
+								</NavLink>
+							</div>
+						</div>
+					);
+				}}
+			</Media>
+		</>
 	);
 };
 
--- src/pages/translatorsComponents/TranslatorsFirstSection.js
+++ src/pages/translatorsComponents/TranslatorsFirstSection.js
@@ -10,7 +10,7 @@ const TranslatorsFirstSection = () => {
 					<h2>Chętnie nawiążemy współpracę</h2>
 				</div>
 				<p className="translators-txt">
-					Biuro Tłumaczeń Przysięgłych„DAG-MAR” skupia wokół siebie zespół
+					Biuro Tłumaczeń Przysięgłych „DAG-MAR” skupia wokół siebie zespół
 					Tłumaczy z wieloletnim doświadczeniem. Wobec stale rosnących wymagań i
 					oczekiwań naszych Klientów decydujemy się na współpracę tylko z
 					prawdziwymi profesjonalistami w swojej branży.
