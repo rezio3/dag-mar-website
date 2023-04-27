3f2f0a6989b6de20df76722da96ee06271aa8bc3
Joszko <jakub.rezler96@gmail.com>
2023-04-27 12:59:13 +0200

    add build-progress span to Offer component

--- src/pages/Offer.js
+++ src/pages/Offer.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "../style/css/offer.css";
 
 const Offer = () => {
-	return <div></div>;
+	return (
+		<div>
+			<span className="build-progress-span">Build in progress...</span>
+		</div>
+	);
 };
 
 export default Offer;
