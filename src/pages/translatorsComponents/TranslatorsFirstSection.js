be02ac805a329f0752ab2743a751d87bcfc3a422
Joszko <jakub.rezler96@gmail.com>
2024-04-27 03:16:19 +0200

    update TranslatorsFirstSection.js component

--- src/pages/translatorsComponents/TranslatorsFirstSection.js
+++ src/pages/translatorsComponents/TranslatorsFirstSection.js
@@ -1,26 +1,30 @@
 import React from "react";
 
 const TranslatorsFirstSection = () => {
-	console.log(document.URL);
-	return (
-		<div className="translators-first-section">
-			<div className="translators-text">
-				<div className="translators-header-container">
-					<h3>Dla tłumaczy</h3>
-					<h2>Chętnie nawiążemy współpracę</h2>
-				</div>
-				<p className="translators-txt">
-					Biuro Tłumaczeń Przysięgłych „DAG-MAR” skupia wokół siebie zespół
-					Tłumaczy z wieloletnim doświadczeniem. Wobec stale rosnących wymagań i
-					oczekiwań naszych Klientów decydujemy się na współpracę tylko z
-					prawdziwymi profesjonalistami w swojej branży.
-				</p>
-			</div>
-			<div className="translators-arc-img">
-				<div className="translators-img" />
-			</div>
-		</div>
-	);
+  return (
+    <div className="translators-first-section">
+      <div className="translators-text">
+        <div className="translators-header-container">
+          <h3>Dla tłumaczy</h3>
+          <h2>Chętnie nawiążemy współpracę</h2>
+        </div>
+        <p className="translators-txt">
+          Biuro Tłumaczeń Przysięgłych „DAG-MAR” skupia wokół siebie zespół
+          Tłumaczy <br />z wieloletnim doświadczeniem. Wobec stale rosnących wymagań i
+          oczekiwań naszych Klientów decydujemy się na współpracę tylko z
+          prawdziwymi profesjonalistami w swojej branży.
+        </p>
+        <p>
+          Zapewniamy materiały do tłumaczenia, uczciwe stawki za wykonaną pracę,
+          rozliczenia na czas, a także rekomendacje i polecenie innym
+          pracodawcom <br />w przyszłości.
+        </p>
+      </div>
+      <div className="translators-arc-img">
+        <div className="translators-img" />
+      </div>
+    </div>
+  );
 };
 
 export default TranslatorsFirstSection;
