c854d17db56a1743e1f7ab37b4dd92792e7fe0f4
Joszko <jakub.rezler96@gmail.com>
2024-07-15 01:28:29 +0200

    update styles

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -7,110 +7,114 @@ import googleIcon from "../img/GoogleIcon.svg";
 import Media from "react-media";
 
 const Footer = () => {
-	return (
-		<>
-			<Media query="(min-width: 992px)">
-				{(matches) => {
-					return matches ? (
-						<footer className="footer-container">
-							<div className="upper-footer-container">
-								<img
-									src={footerLogo}
-									alt="dag-mar logo"
-									className="footer-logo"
-								/>
-								<div className="footer-address">
-									<h2>Biuro Tłumaczeń Przysięgłych DAG-MAR</h2>
-									<span className="footer-contact-number">+48 607 618 567</span>
-									<span>Karkonoska 1C, 59-300 Lubin</span>
-								</div>
-								<div className="aboutUs-offer-FAQ-translators-footer aboutUs-footer">
-									<NavLink to="/" end className="nav-btn-desktop">
-										<span className="footer-link">O nas</span>
-									</NavLink>
-									<NavLink to="/offer" className="nav-btn-desktop">
-										<span className="footer-link">Oferta</span>
-									</NavLink>
-									<span className="footer-link">Płatności</span>
-								</div>
-								<div className="aboutUs-offer-FAQ-translators-footer">
-									<NavLink to="/fortranslators" className="nav-btn-desktop">
-										<span className="footer-link">Dla Tłumaczy</span>
-									</NavLink>
-									<NavLink to="/faq" className="nav-btn-desktop">
-										<span className="footer-link">FAQ</span>
-									</NavLink>
-								</div>
+  return (
+    <>
+      <Media query="(min-width: 992px)">
+        {(matches) => {
+          return matches ? (
+            <footer className="footer-container">
+              <div className="upper-footer-container">
+                <img
+                  src={footerLogo}
+                  alt="dag-mar logo"
+                  className="footer-logo"
+                />
+                <div className="footer-address">
+                  <h2>Biuro Tłumaczeń Przysięgłych DAG-MAR</h2>
+                  <span className="footer-contact-number">+48 607 618 567</span>
+                  <span>Karkonoska 1C, 59-300 Lubin</span>
+                </div>
+                <div className="aboutUs-offer-FAQ-translators-footer aboutUs-footer">
+                  <NavLink to="/" end className="nav-btn-desktop">
+                    <span className="footer-link">O nas</span>
+                  </NavLink>
+                  <NavLink to="/offer" className="nav-btn-desktop">
+                    <span className="footer-link">Oferta</span>
+                  </NavLink>
+                  <NavLink to="/contact" className="nav-btn-desktop">
+                    <span className="footer-link">Płatności</span>
+                  </NavLink>
+                </div>
+                <div className="aboutUs-offer-FAQ-translators-footer">
+                  <NavLink to="/fortranslators" className="nav-btn-desktop">
+                    <span className="footer-link">Dla Tłumaczy</span>
+                  </NavLink>
+                  <NavLink to="/faq" className="nav-btn-desktop">
+                    <span className="footer-link">FAQ</span>
+                  </NavLink>
+                </div>
 
-								<NavLink to="/contact" className="nav-btn-desktop">
-									<button className="footer-contact-btn">Skontaktuj się</button>
-								</NavLink>
-							</div>
-							<div className="lower-footer-container">
-								<span>Copyright &copy; 2022 DAG-MAR</span>
-								<div className="icons-footer-container">
-									<img src={fbIcon} alt="Facebook Icon" className="icon" />
-									<img src={googleIcon} alt="Google Icon" className="icon" />
-								</div>
-								<span>Polityka prywatności</span>
-							</div>
-						</footer>
-					) : (
-						<footer className="footer-container">
-							<div className="upper-footer-container">
-								<div className="footer-logo-container">
-									<img
-										src={footerLogo}
-										alt="dag-mar logo"
-										className="footer-logo"
-									/>
-									<div className="footer-address">
-										<h2>Biuro Tłumaczeń Przysięgłych DAG-MAR</h2>
-										<span className="footer-contact-number">
-											+48 607 618 567
-										</span>
-										<span>Karkonoska 1C, 59-300 Lubin</span>
-									</div>
-								</div>
-								<div className="middle-footer-container">
-									<div className="aboutUs-offer-FAQ-translators-footer aboutUs-footer">
-										<NavLink to="/" end className="nav-btn-desktop">
-											<span className="footer-link">O nas</span>
-										</NavLink>
-										<NavLink to="/offer" className="nav-btn-desktop">
-											<span className="footer-link">Oferta</span>
-										</NavLink>
-										<NavLink to="/fortranslators" className="nav-btn-desktop">
-											<span className="footer-link">Dla Tłumaczy</span>
-										</NavLink>
-									</div>
-									<div className="aboutUs-offer-FAQ-translators-footer aboutUs-right">
-										<span className="footer-link">Płatności</span>
+                <NavLink to="/contact" className="nav-btn-desktop">
+                  <button className="footer-contact-btn">Skontaktuj się</button>
+                </NavLink>
+              </div>
+              <div className="lower-footer-container">
+                <span>Copyright &copy; 2022 DAG-MAR</span>
+                <div className="icons-footer-container">
+                  <img src={fbIcon} alt="Facebook Icon" className="icon" />
+                  <img src={googleIcon} alt="Google Icon" className="icon" />
+                </div>
+                <span>Polityka prywatności</span>
+              </div>
+            </footer>
+          ) : (
+            <footer className="footer-container">
+              <div className="upper-footer-container">
+                <div className="footer-logo-container">
+                  <img
+                    src={footerLogo}
+                    alt="dag-mar logo"
+                    className="footer-logo"
+                  />
+                  <div className="footer-address">
+                    <h2>Biuro Tłumaczeń Przysięgłych DAG-MAR</h2>
+                    <span className="footer-contact-number">
+                      +48 607 618 567
+                    </span>
+                    <span>Karkonoska 1C, 59-300 Lubin</span>
+                  </div>
+                </div>
+                <div className="middle-footer-container">
+                  <div className="aboutUs-offer-FAQ-translators-footer aboutUs-footer">
+                    <NavLink to="/" end className="nav-btn-desktop">
+                      <span className="footer-link">O nas</span>
+                    </NavLink>
+                    <NavLink to="/offer" className="nav-btn-desktop">
+                      <span className="footer-link">Oferta</span>
+                    </NavLink>
+                    <NavLink to="/fortranslators" className="nav-btn-desktop">
+                      <span className="footer-link">Dla Tłumaczy</span>
+                    </NavLink>
+                  </div>
+                  <div className="aboutUs-offer-FAQ-translators-footer aboutUs-right">
+                    <NavLink to="/contact" className="nav-btn-desktop">
+                      <span className="footer-link">Płatności</span>
+                    </NavLink>
 
-										<NavLink to="/faq" className="nav-btn-desktop">
-											<span className="footer-link">FAQ</span>
-										</NavLink>
-									</div>
-								</div>
-								<NavLink to="/contact" className="nav-btn-desktop">
-									<button className="footer-contact-btn">Skontaktuj się</button>
-								</NavLink>
-							</div>
-							<div className="lower-footer-container">
-								<div className="icons-footer-container">
-									<img src={fbIcon} alt="Facebook Icon" className="icon" />
-									<img src={googleIcon} alt="Google Icon" className="icon" />
-								</div>
+                    <NavLink to="/faq" className="nav-btn-desktop">
+                      <span className="footer-link">FAQ</span>
+                    </NavLink>
+                  </div>
+                </div>
+                <NavLink to="/contact" className="nav-btn-desktop">
+                  <button className="footer-contact-btn">Skontaktuj się</button>
+                </NavLink>
+              </div>
+              <div className="lower-footer-container">
+                <div className="icons-footer-container">
+                  <img src={fbIcon} alt="Facebook Icon" className="icon" />
+                  <img src={googleIcon} alt="Google Icon" className="icon" />
+                </div>
 
-								<span>Polityka prywatności</span>
-								<span>Copyright &copy; 2022 DAG-MAR</span>
-							</div>
-						</footer>
-					);
-				}}
-			</Media>
-		</>
-	);
+                <span>Polityka prywatności</span>
+                <span>Copyright &copy; 2022 DAG-MAR</span>
+              </div>
+            </footer>
+          );
+        }}
+      </Media>
+    </>
+  );
 };
 
 export default Footer;
--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -7,33 +7,33 @@ import Faq from "../pages/Faq";
 import Contact from "../pages/Contact";
 
 const Page = () => {
-	window.onscroll = () => {
-		const myNav = document.getElementById("nav");
-		const logo = document.getElementById("logo");
-		if (window.scrollY > 50) {
-			myNav.classList.add("nav-scrolled");
-			myNav.classList.remove("nav-up");
-			logo.classList.add("logo-scrolled");
-			logo.classList.remove("logo-up");
-		} else {
-			myNav.classList.add("nav-up");
-			myNav.classList.remove("nav-scrolled");
-			logo.classList.add("logo-up");
-			logo.classList.remove("logo-scrolled");
-		}
-	};
+  window.onscroll = () => {
+    const myNav = document.getElementById("nav");
+    const logo = document.getElementById("logo");
+    if (window.scrollY > 50) {
+      myNav.classList.add("nav-scrolled");
+      myNav.classList.remove("nav-up");
+      logo.classList.add("logo-scrolled");
+      logo.classList.remove("logo-up");
+    } else {
+      myNav.classList.add("nav-up");
+      myNav.classList.remove("nav-scrolled");
+      logo.classList.add("logo-up");
+      logo.classList.remove("logo-scrolled");
+    }
+  };
 
-	return (
-		<div className="page-container">
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/offer" element={<Offer />} />
-				<Route path="/fortranslators" element={<Fortranslators />} />
-				<Route path="/faq" element={<Faq />} />
-				<Route path="/contact" element={<Contact />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="page-container">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/offer" element={<Offer />} />
+        <Route path="/fortranslators" element={<Fortranslators />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Page;
