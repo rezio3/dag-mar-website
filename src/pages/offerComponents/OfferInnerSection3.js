0c78610c301b3da286f4e26900eec92531e96800
Jakub Rezler <jakub.rezler96@gmail.com>
2025-10-22 12:32:04 +0200

    update index.html

--- src/pages/offerComponents/OfferInnerSection3.js
+++ src/pages/offerComponents/OfferInnerSection3.js
@@ -9,11 +9,11 @@ const OfferInnerSection3 = () => {
             Jeśli chcą Państwo otrzymać bezpłatną wycenę tłumaczenia, prosimy
             o&nbsp;opisanie jakich dokumentów dotyczyłoby to tłumaczenie lub też
             o&nbsp;przesłanie tekstu na naszą pocztę elektroniczną przy
-            pomocy formularza wyceny. Prosimy o&nbsp;określenie terminu, na
-            który zlecenie miałoby zostać wykonane, a&nbsp;także o&nbsp;podanie
-            danych kontaktowych. Otrzymanie wyceny nie zobowiązuje Państwa do
-            podpisania umowy, nie jest też równoznaczne z&nbsp;przyjęciem przez
-            nas zlecenia.
+            pomocy formularza kontaktowego. Prosimy o&nbsp;określenie terminu,
+            na który zlecenie miałoby zostać wykonane, a&nbsp;także
+            o&nbsp;podanie danych kontaktowych. Otrzymanie wyceny nie
+            zobowiązuje Państwa do podpisania umowy, nie jest też równoznaczne
+            z&nbsp;przyjęciem przez nas zlecenia.
           </span>
         </div>
       </div>
